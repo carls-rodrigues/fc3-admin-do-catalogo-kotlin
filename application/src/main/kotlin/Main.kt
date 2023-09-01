85a39a594fa7e6f44eaea4ccbbc211785d738d61
Carlos Rodrigues <cerf@furg.br>
2023-09-01 17:28:10 -0300

    chore: create modules

---
+++ application/src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+package com.fullcycle.admin.catalogo.application
+import com.fullcycle.admin.catalogo.domain.Category
+fun main() {
+    var category = Category()
+    println("Hello World!")
+}
---
+++ domain/src/main/kotlin/Category.kt
@@ -0,0 +1,3 @@
+package com.fullcycle.admin.catalogo.domain
+
+public class Category {}
