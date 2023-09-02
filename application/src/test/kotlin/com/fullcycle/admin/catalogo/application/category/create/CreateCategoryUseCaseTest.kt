843342416131580be8bc0b24e08642cfd8f9452a
Carlos Rodrigues <cerf@furg.br>
2023-09-02 19:29:32 -0300

    test: ensure CreateCategoryUseCase throws at invalid or random errors

--- application/src/test/kotlin/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.kt
+++ application/src/test/kotlin/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.kt
@@ -1,6 +1,7 @@
 package com.fullcycle.admin.catalogo.application.category.create
 
 import com.fullcycle.admin.catalogo.domain.category.CategoryGateway
+import com.fullcycle.admin.catalogo.domain.exceptions.DomainException
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -55,9 +56,33 @@ class CreateCategoryUseCaseTest {
 
     @Test
     fun given_an_invalid_name_when_call_create_category_then_should_return_DomainException() {
-        val expectedName = "Filmes"
+        val expectedName = null
         val expectedDescription = "A categoria mais assistida"
         val expectedIsActive = true
+        val expectedErrorMessage = "'name' should not be null"
+        val expectedErrorCount = 1
+
+        val command = CreateCategoryCommand.with(
+            name = expectedName,
+            description = expectedDescription,
+            isActive = expectedIsActive
+        )
+
+        val actualException = Assertions.assertThrows(DomainException::class.java) {
+            useCase.execute(command)
+        }
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors()[0].message)
+
+        verify(categoryGateway, times(0)).create(any())
+    }
+
+    @Test
+    fun given_an_valid_command_with_inactive_category_when_call_create_category_then_should_return_an_inactive_category_id() {
+        // given
+        val expectedName = "Filmes"
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = false
 
         val command = CreateCategoryCommand.with(
             name = expectedName,
@@ -69,6 +94,55 @@ class CreateCategoryUseCaseTest {
             .thenAnswer(returnsFirstArg<CategoryGateway>())
 
         val actualOutput = useCase.execute(command)
+
+        Assertions.assertNotNull(actualOutput)
+        Assertions.assertNotNull(actualOutput.id)
+
+        verify(categoryGateway, times(1))
+            .create(argThat { aCategory ->
+                Objects.equals(expectedName, aCategory.getName())
+                        && Objects.equals(expectedDescription, aCategory.getDescription())
+                        && Objects.equals(expectedIsActive, aCategory.isActive())
+                        && Objects.nonNull(aCategory.getId())
+                        && Objects.nonNull(aCategory.getCreatedAt())
+                        && Objects.nonNull(aCategory.getUpdatedAt())
+                        && Objects.nonNull(aCategory.getDeletedAt())
+            })
+    }
+
+    @Test
+    fun given_an_valid_command_when_gateway_throws_random_exception_then_should_return_a_exception() {
+        // given
+        val expectedName = "Filmes"
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = true
+        val expectedErrorMessage = "Gateway error"
+
+        val command = CreateCategoryCommand.with(
+            name = expectedName,
+            description = expectedDescription,
+            isActive = expectedIsActive
+        )
+
+        whenever(categoryGateway.create(any()))
+            .thenThrow(IllegalStateException(expectedErrorMessage))
+
+        val actualException = Assertions.assertThrows(IllegalStateException::class.java) {
+            useCase.execute(command)
+        }
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.message)
+
+        verify(categoryGateway, times(1))
+            .create(argThat { aCategory ->
+                Objects.equals(expectedName, aCategory.getName())
+                        && Objects.equals(expectedDescription, aCategory.getDescription())
+                        && Objects.equals(expectedIsActive, aCategory.isActive())
+                        && Objects.nonNull(aCategory.getId())
+                        && Objects.nonNull(aCategory.getCreatedAt())
+                        && Objects.nonNull(aCategory.getUpdatedAt())
+                        && Objects.isNull(aCategory.getDeletedAt())
+            })
     }
 
 }
