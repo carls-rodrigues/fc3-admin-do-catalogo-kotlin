02a673a28f8bfe084c6f0aa0ead2b7f623bfd6b1
Carlos Rodrigues <cerf@furg.br>
2023-09-01 21:35:33 -0300

    test: ensure Category name does not accept null values

--- domain/src/test/kotlin/com/fullcycle/admin/catalogo/domain/category/CategoryTest.kt
+++ domain/src/test/kotlin/com/fullcycle/admin/catalogo/domain/category/CategoryTest.kt
@@ -1,5 +1,7 @@
 package com.fullcycle.admin.catalogo.domain.category
 
+import com.fullcycle.admin.catalogo.domain.exceptions.DomainException
+import com.fullcycle.admin.catalogo.domain.validation.handler.ThrowsValidationHandler
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
@@ -18,11 +20,32 @@ class CategoryTest {
 
         Assertions.assertNotNull(actualCategory)
         Assertions.assertNotNull(actualCategory.getId())
-        Assertions.assertEquals(expectedName, actualCategory.name())
+        Assertions.assertEquals(expectedName, actualCategory.getName())
         Assertions.assertEquals(expectedDescription, actualCategory.getDescription())
         Assertions.assertEquals(expectedIsActive, actualCategory.isActive())
         Assertions.assertNotNull(actualCategory.getCreatedAt())
         Assertions.assertNotNull(actualCategory.getUpdatedAt())
         Assertions.assertNull(actualCategory.getDeletedAt())
+    }
+    @Test
+    fun given_an_invalid_null_name_when_call_new_category_and_validate_then_should_receive_error() {
+        val expectedName = null
+        val expectedErrorMessage = "'name' should not be null"
+        val expectedErrorCount =  1
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = true
+
+        val actualCategory = Category.newCategory(
+            name = expectedName,
+            description = expectedDescription,
+            isActive = expectedIsActive
+        )
+        val handler = ThrowsValidationHandler()
+        val actualException = Assertions.assertThrows(DomainException::class.java) {
+            actualCategory.validate(handler)
+        }
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size)
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors()[0].message)
     }
 }
