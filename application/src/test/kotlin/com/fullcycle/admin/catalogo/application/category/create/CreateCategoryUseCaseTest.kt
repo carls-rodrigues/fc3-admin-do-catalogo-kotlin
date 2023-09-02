a79d8f71f7b4b12185b3a49e7ed9cf9205be6b75
Carlos Rodrigues <cerf@furg.br>
2023-09-02 18:54:55 -0300

    test: refactor CategoryUseCaseTest to mock instances

--- application/src/test/kotlin/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.kt
+++ application/src/test/kotlin/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.kt
@@ -3,11 +3,23 @@ package com.fullcycle.admin.catalogo.application.category.create
 import com.fullcycle.admin.catalogo.domain.category.CategoryGateway
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.AdditionalAnswers.returnsFirstArg
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.*
 import java.util.*
 
+
+@ExtendWith(MockitoExtension::class)
 class CreateCategoryUseCaseTest {
+
+    @Mock
+    private lateinit var categoryGateway: CategoryGateway;
+
+    @InjectMocks
+    private lateinit var useCase: DefaultCreateCategoryUseCase;
     @Test
     fun given_an_valid_command_when_call_create_category_then_should_return_category_id() {
         // given
@@ -20,10 +32,9 @@ class CreateCategoryUseCaseTest {
             description = expectedDescription,
             isActive = expectedIsActive
         )
-        val categoryGateway = mock<CategoryGateway>() {
-            on { create(any()) }.thenAnswer(returnsFirstArg<CategoryGateway>())
-        }
-        val useCase = DefaultCreateCategoryUseCase(categoryGateway)
+
+        whenever(categoryGateway.create(any()))
+            .thenAnswer(returnsFirstArg<CategoryGateway>())
 
         val actualOutput = useCase.execute(command)
 
@@ -42,5 +53,22 @@ class CreateCategoryUseCaseTest {
             })
     }
 
+    @Test
+    fun given_an_invalid_name_when_call_create_category_then_should_return_DomainException() {
+        val expectedName = "Filmes"
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = true
+
+        val command = CreateCategoryCommand.with(
+            name = expectedName,
+            description = expectedDescription,
+            isActive = expectedIsActive
+        )
+
+        whenever(categoryGateway.create(any()))
+            .thenAnswer(returnsFirstArg<CategoryGateway>())
+
+        val actualOutput = useCase.execute(command)
+    }
 
 }
