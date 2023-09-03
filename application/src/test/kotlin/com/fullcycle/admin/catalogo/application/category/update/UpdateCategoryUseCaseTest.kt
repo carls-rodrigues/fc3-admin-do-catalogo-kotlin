16c57bf32ee078560b972c1dfde2b575d585d614
Carlos Rodrigues <cerf@furg.br>
2023-09-03 14:39:50 -0300

    test: ensure UpdateCategoryUseCase throws an DomainException if id is invalid or NotFound

--- application/src/test/kotlin/com/fullcycle/admin/catalogo/application/category/update/UpdateCategoryUseCaseTest.kt
+++ application/src/test/kotlin/com/fullcycle/admin/catalogo/application/category/update/UpdateCategoryUseCaseTest.kt
@@ -2,6 +2,8 @@ package com.fullcycle.admin.catalogo.application.category.update
 
 import com.fullcycle.admin.catalogo.domain.category.Category
 import com.fullcycle.admin.catalogo.domain.category.CategoryGateway
+import com.fullcycle.admin.catalogo.domain.category.CategoryID
+import com.fullcycle.admin.catalogo.domain.exceptions.DomainException
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -168,6 +170,39 @@ class UpdateCategoryUseCaseTest {
                         && category.getUpdatedAt().isBefore(aUpdatedCategory.getUpdatedAt())
                         && Objects.isNull(aUpdatedCategory.getDeletedAt())
             })
+    }
+
+    @Test
+    fun given_a_command_with_invalid_id_when_calls_update_category_then_should_return_not_found_exception() {
+        val category = Category.newCategory("Film", null, true)
+
+        val expectedName = "Filmes"
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = false
+        val expectedId = "123"
+
+        val expectedErrorMessage = "Category with ID %s was not found".format(expectedId)
+        val expectedErrorCount = 1
+
+
+        val command = UpdateCategoryCommand.with(
+            expectedId,
+            expectedName,
+            expectedDescription,
+            expectedIsActive
+        )
+        whenever(categoryGateway.findByID(eq(CategoryID.from(expectedId))))
+            .thenReturn(null)
+
+        val actualException = Assertions.assertThrows(DomainException::class.java) {
+            useCase.execute(command)
+        }
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size)
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors()[0].message)
+
+        verify(categoryGateway, times(1)).findByID(eq(CategoryID.from(expectedId)))
+        verify(categoryGateway, times(0)).update(any())
     }
 
 }
