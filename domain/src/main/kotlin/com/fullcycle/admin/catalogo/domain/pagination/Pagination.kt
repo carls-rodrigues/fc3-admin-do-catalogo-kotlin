0eb11008255d3d40f2371df484ead6357b461f2a
Carlos Rodrigues <cerf@furg.br>
2023-09-03 19:17:13 -0300

    feat: add mapper to Pagination

--- domain/src/main/kotlin/com/fullcycle/admin/catalogo/domain/pagination/Pagination.kt
+++ domain/src/main/kotlin/com/fullcycle/admin/catalogo/domain/pagination/Pagination.kt
@@ -4,5 +4,10 @@ data class Pagination<T>(
     val currentPage: Int,
     val perPage: Int,
     val total: Long,
-    val list: List<T>
-)
+    val items: List<T>
+) {
+    fun <R> map(mapper: (T) -> R): Pagination<R> {
+        val newList = this.items.stream().map(mapper).toList()
+        return Pagination(this.currentPage, this.perPage, this.total, newList)
+    }
+}
