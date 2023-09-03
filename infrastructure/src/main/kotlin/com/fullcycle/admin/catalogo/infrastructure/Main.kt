959a46f7bccfa1023c2bc103ab1d797cbb2e9581
Carlos Rodrigues <cerf@furg.br>
2023-09-03 15:58:01 -0300

    refactor: remove invalid instance of UseCase

--- infrastructure/src/main/kotlin/com/fullcycle/admin/catalogo/infrastructure/Main.kt
+++ infrastructure/src/main/kotlin/com/fullcycle/admin/catalogo/infrastructure/Main.kt
@@ -1,8 +1,5 @@
 package com.fullcycle.admin.catalogo.infrastructure
 
-import com.fullcycle.admin.catalogo.application.UseCase
-
 fun main(args: Array<String>) {
     println("Starting application")
-    println(UseCase().execute())
 }
