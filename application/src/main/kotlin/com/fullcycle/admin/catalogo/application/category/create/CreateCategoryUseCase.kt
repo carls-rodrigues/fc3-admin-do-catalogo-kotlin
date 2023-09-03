82f412ba8b8a7b41507d7a66163112cb45961a6d
Carlos Rodrigues <cerf@furg.br>
2023-09-02 21:33:26 -0300

    refactor: add Either to handler success and errors when returning

--- application/src/main/kotlin/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCase.kt
+++ application/src/main/kotlin/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCase.kt
@@ -1,5 +1,7 @@
 package com.fullcycle.admin.catalogo.application.category.create
 
 import com.fullcycle.admin.catalogo.application.UseCase
+import com.fullcycle.admin.catalogo.domain.validation.handler.Notification
+import io.vavr.control.Either
 
-abstract class CreateCategoryUseCase : UseCase<CreateCategoryCommand, CreateCategoryOutput>()
+abstract class CreateCategoryUseCase : UseCase<CreateCategoryCommand, Either<Notification, CreateCategoryOutput>>()
